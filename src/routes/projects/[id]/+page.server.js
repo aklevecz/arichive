cde3f563b723eea2a23b9c5db74f5d0432b7b16c
ariel klevecz <ariel@yaytso.art>
2024-08-11 09:01:00 -0700

    prerenders

--- src/routes/projects/[id]/+page.server.js
+++ src/routes/projects/[id]/+page.server.js
@@ -1,13 +1,20 @@
 import { projectsData } from '$lib/stores/projects.svelte';
 import { error } from '@sveltejs/kit';
 
-export const prerender = true
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+	const projectIds = projectsData.map((project) => ({
+		id: project.id
+	}));
+	return projectIds;
+}
+
+export const prerender = true;
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
-    
-    const project = projectsData.find(project => project.id === params.id)
-    if (!project) {
-        error(404, "Project not found")
-    }
+	const project = projectsData.find((project) => project.id === params.id);
+	if (!project) {
+		error(404, 'Project not found');
+	}
 	return { id: params.id, project };
 }
