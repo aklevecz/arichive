29829a50e565719b802ccd5c198f6a4c68fb9e27
ariel klevecz <ariel@yaytso.art>
2025-06-20 13:11:29 -0700

    trigger redirect

--- src/routes/projects/[id]/+layout.server.js
+++ src/routes/projects/[id]/+layout.server.js
@@ -1,10 +1,14 @@
 import { projectsData } from '$lib/stores/projects.svelte';
-import { error } from 'console';
+import { error, redirect } from '@sveltejs/kit';
 
 export const prerender = true;
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({params}) {
+	if (params.id === 'yaytso-gallery') {
+		throw redirect(301, '/projects/yaytso');
+	}
+	
 	const project = projectsData.find((project) => project.id === params.id);
 	if (!project) {
 		error(404, 'Project not found');
